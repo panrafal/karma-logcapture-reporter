37d9cc028f1ee6bb82f0f00cabb2d799319b31a5
Rafal Lindemann <rl@stamina.pl>
2016-03-12 19:06:06 +0100

    Add colors, logCapture:show option to log always show certain log levels, make it compatible with mocha and mocha-own reporters

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 var util = require('util');
+var chalk = require('chalk');
 
 var LogCaptureReporter = function (config, baseReporterDecorator, emitter) {
 
     this.USE_COLORS = !!config.colors;
+    var options = config.logCapture || {}
+    var showLevel = options.show
 
     baseReporterDecorator(this);
 
@@ -20,17 +23,62 @@ var LogCaptureReporter = function (config, baseReporterDecorator, emitter) {
             origBrowserLog.call(this, browser, log, type);
         }
 
+        var show = false
+        if (config.colors) {
+            switch (type) {
+                case 'error':
+                    log = chalk.red.inverse.bold(' ERR ') + ' ' + chalk.reset.dim(log)
+                    break;
+                case 'warn':
+                    show = showLevel === 'warn' || showLevel === 'info' || showLevel === 'log'
+                    log = chalk.yellow.inverse.bold(' WRN ') + ' ' + chalk.reset.dim(log)
+                    break;
+                case 'info':
+                    show = showLevel === 'info' || showLevel === 'log'
+                    log = chalk.blue.inverse.bold(' INF ') + ' ' + chalk.reset.dim(log)
+                    break;
+                default:
+                    show = showLevel === 'log'
+                    log = chalk.grey.inverse.bold(' LOG ') + ' ' + chalk.grey(log)
+                    break;
+            }
+        } else {
+            log = type.toUpperCase() + ': ' + log
+        }
+
+        switch (type) {
+            case 'error':
+                show = showLevel === 'error' || showLevel === 'warn' || showLevel === 'info' || showLevel === 'log'
+                break;
+            case 'warn':
+                show = showLevel === 'warn' || showLevel === 'info' || showLevel === 'log'
+                break;
+            case 'info':
+                show = showLevel === 'info' || showLevel === 'log'
+                break;
+            default:
+                show = showLevel === 'log'
+                break;
+        }
+
+        if (show) {
+            console.log('  ' + log)
+        }
+
         this.captured.push(
-            type.toUpperCase() + ': ' + log
+            log
         );
     };
 
     this.onSpecComplete = function (browser, result) {
-
         if (!result.success && !result.skipped && this.captured.length) {
-            result.log.push(
-                '\nCaptured logs:\n  ' + this.captured.join('\n  ')
-            );
+            result.log = [
+               result.log.join('\n') +
+               '\n\n' +
+               (config.colors ? chalk.white('Captured logs:  ') : 'Captured logs:  ') +
+               '\n\n  ' +
+               this.captured.join('\n  ')
+            ]// );
         }
 
         this.captured = [];
